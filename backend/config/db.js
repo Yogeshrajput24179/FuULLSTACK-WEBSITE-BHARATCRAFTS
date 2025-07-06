404e039efea81a2eea211920d52c6e0fa176f11b
YOGESH <111962172+Yogeshrajput24179@users.noreply.github.com>
2025-07-06 19:22:20 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGO_URI;
 
 if (!uri) {
   console.error("MongoDB URI is missing. Check your .env file.");
