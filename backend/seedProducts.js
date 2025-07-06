57fd19b9d406fab0688d1cac5d370bd114d3fd48
YOGESH <111962172+Yogeshrajput24179@users.noreply.github.com>
2025-07-06 20:09:09 +0530

    Update seedProducts.js

--- backend/seedProducts.js
+++ backend/seedProducts.js
@@ -1,7 +1,7 @@
-// backend/seedProducts.js
+// backend/seed`${baseImageUrl}/Products'.js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import ProductModel from "./models/productModel.js";
+import `${baseImageUrl}/ProductM'odel from "./models/productModel.js";
 
 dotenv.config();
 
@@ -11,7 +11,7 @@ const productList = [
  {
         id: "1",
         name: "Wall Decor",
-        image: Product1,
+        image: `${baseImageUrl}/Product1',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -19,7 +19,7 @@ const productList = [
     {
         id: "2",
         name: "Plant Stand",
-        image: Product2,
+        image: `${baseImageUrl}/Product2',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -28,7 +28,7 @@ const productList = [
     {
         id: "3",
         name: "Wall Clock",
-        image: Product3,
+        image: `${baseImageUrl}/Product3',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -37,7 +37,7 @@ const productList = [
     {
         id: "4",
         name: "Table",
-        image: Product4,
+        image: `${baseImageUrl}/Product4',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -46,7 +46,7 @@ const productList = [
     {
         id: "5",
         name: "Study Table",
-        image: Product5,
+        image: `${baseImageUrl}/Product5',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -55,7 +55,7 @@ const productList = [
     {
         id: "6",
         name: "Self",
-        image: Product6,
+        image: `${baseImageUrl}/Product6',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -64,7 +64,7 @@ const productList = [
     {
         id: "7",
         name: "Lamp",
-        image: Product7,
+        image: `${baseImageUrl}/Product7',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -73,7 +73,7 @@ const productList = [
     {
         id: "8",
         name: "Key Holder",
-        image: Product8,
+        image: `${baseImageUrl}/Product8',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -82,7 +82,7 @@ const productList = [
     {
         id: "9",
         name: "Dining Table",
-        image: Product9,
+        image: `${baseImageUrl}/Product9',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -91,7 +91,7 @@ const productList = [
     {
         id: "10",
         name: "Mug Holder",
-        image: Product10,
+        image: `${baseImageUrl}/Product10',
         price: 12,
         description: "Discover our stylish and functional furniture collection, offering comfort, durability, and modern designs to enhance every room in your home.",
         category: "Furniture"
@@ -100,14 +100,14 @@ const productList = [
     {
         id: "11",
         name: "Clay lamp",
-        image: Product11,
+        image: `${baseImageUrl}/Product11',
         price: 10,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
     }, {
         id: "12",
         name: "Clay pan",
-        image: Product12,
+        image: `${baseImageUrl}/Product12',
         price: 12,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
@@ -115,7 +115,7 @@ const productList = [
     {
         id: "13",
         name: "Clay Pan",
-        image: Product13,
+        image: `${baseImageUrl}/Product13',
         price: 12,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
@@ -123,77 +123,77 @@ const productList = [
     {
         id: "14",
         name: "Clay handi",
-        image: Product14,
+        image: `${baseImageUrl}/Product14',
         price: 18,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
     }, {
         id: "15",
         name: "Clay Pot",
-        image: Product15,
+        image: `${baseImageUrl}/Product15',
         price: 16,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
     }, {
         id: "16",
         name: "Clay Bowl",
-        image: Product16,
+        image: `${baseImageUrl}/Product16',
         price: 24,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
     }, {
         id: "17",
         name: "Clay Diya",
-        image: Product17,
+        image: `${baseImageUrl}/Product17',
         price: 14,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
     }, {
         id: "18",
         name: "Shirt",
-        image: Product18,
+        image: `${baseImageUrl}/Product18',
         price: 12,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "19",
         name: "Pillo Cover",
-        image: Product19,
+        image: `${baseImageUrl}/Product19',
         price: 20,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "20",
         name: "Dhoti Kurta",
-        image: Product20,
+        image: `${baseImageUrl}/Product20',
         price: 15,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "21",
         name: "Clay jar",
-        image: Product21,
+        image: `${baseImageUrl}/Product21',
         price: 14,
         description: "Beautiful handcrafted pottery items for decor, utility, and gifting",
         category: "Pottery"
     }, {
         id: "22",
         name: "Saree",
-        image: Product22,
+        image: `${baseImageUrl}/Product22',
         price: 22,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "23",
         name: "Pillo Cover",
-        image: Product23,
+        image: `${baseImageUrl}/Product23',
         price: 10,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "24",
         name: "Saree",
-        image: Product24,
+        image: `${baseImageUrl}/Product24',
         price: 12,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
@@ -201,7 +201,7 @@ const productList = [
     {
         id: "25",
         name: "Banarsi Saree",
-        image: Product25,
+        image: `${baseImageUrl}/Product25',
         price: 12,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
@@ -209,49 +209,49 @@ const productList = [
     {
         id: "26",
         name: "Palin Kurta",
-        image: Product26,
+        image: `${baseImageUrl}/Product26',
         price: 18,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "27",
         name: "OverSize Shirt",
-        image: Product27,
+        image: `${baseImageUrl}/Product27',
         price: 16,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "28",
         name: "Badsheet",
-        image: Product28,
+        image: `${baseImageUrl}/Product28',
         price: 24,
         description: "Exquisite handmade textile products crafted with care",
         category: "Textile"
     }, {
         id: "29",
         name: "BAmboo Pot",
-        image: Product29,
+        image: `${baseImageUrl}/Product29',
         price: 14,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
     }, {
         id: "30",
         name: "Pecock painting",
-        image: Product30,
+        image: `${baseImageUrl}/Product30',
         price: 12,
         description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
         category: "Painting"
     }, {
         id: "31",
         name: "Home Decoration",
-        image: Product31,
+        image: `${baseImageUrl}/Product31',
         price: 20,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
     }, {
         id: "32",
         name: "Bandwar",
-        image: Product32,
+        image: `${baseImageUrl}/Product32',
         price: 15,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
@@ -260,7 +260,7 @@ const productList = [
     {
         id: "33",
         name: "Doll",
-        image: Product33,
+        image: `${baseImageUrl}/Product33',
         price: 18,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
@@ -270,7 +270,7 @@ const productList = [
     {
         id: "35",
         name: "Neckles",
-        image: Product35,
+        image: `${baseImageUrl}/Product35',
         price: 22,
         description: "Unique and elegant handmade jewelry crafted with love and attention to detail.",
         category: "Jwellery"
@@ -278,14 +278,14 @@ const productList = [
     {
         id: "36",
         name: "Home Decor item",
-        image: Product36,
+        image: `${baseImageUrl}/Product36',
         price: 25,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.Jute"
     },
     {
         id: "37",
         name: "Nature Painting",
-        image: Product37,
+        image: `${baseImageUrl}/Product37',
         price: 30,
         description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai.",
         category: "Painting"
@@ -293,7 +293,7 @@ const productList = [
     {
         id: "38",
         name: "cloth Necless",
-        image: Product38,
+        image: `${baseImageUrl}/Product38',
         price: 20,
         description: "Unique and elegant handmade jewelry crafted with love and attention to detail.",
         category: "Jwellery"
@@ -301,7 +301,7 @@ const productList = [
     {
         id: "39",
         name: "Daarwar",
-        image: Product39,
+        image: `${baseImageUrl}/Product39',
         price: 18,
         description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai.",
         category: "Painting"
@@ -309,14 +309,14 @@ const productList = [
     {
         id: "40",
         name: "jute Guitar",
-        image: Product40,
+        image: `${baseImageUrl}/Product40',
         price: 28,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.ute"
     },
     {
         id: "41",
         name: "Name Plate",
-        image: Product41,
+        image: `${baseImageUrl}/Product41',
         price: 25,
         description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
         category: "Painting"
@@ -324,14 +324,14 @@ const productList = [
     {
         id: "42",
         name: "Earings",
-        image: Product42,
+        image: `${baseImageUrl}/Product42',
         price: 32,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.te"
     },
     {
         id: "50",
         name: "Jute Mirror",
-        image: Product50,
+        image: `${baseImageUrl}/Product50',
         price: 20,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.boo & Jute",
         category: "Bamboo & Jute"
@@ -339,7 +339,7 @@ const productList = [
     {
         id: "51",
         name: "Pen Holder",
-        image: Product51,
+        image: `${baseImageUrl}/Product51',
         price: 22,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
@@ -347,7 +347,7 @@ const productList = [
     {
         id: "52",
         name: "Pen Holder",
-        image: Product52,
+        image: `${baseImageUrl}/Product52',
         price: 18,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
@@ -355,14 +355,14 @@ const productList = [
     {
         id: "53",
         name: "Photo Frame",
-        image: Product53,
+        image: `${baseImageUrl}/Product53',
         price: 25,
         description: "blending tradition with sustainability for a natural touch to your lifestyle",
     },
     {
         id: "54",
         name: "bull Painting",
-        image: Product54,
+        image: `${baseImageUrl}/Product54',
         price: 20,
         description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
         category: "Painting"
@@ -370,7 +370,7 @@ const productList = [
     {
         id: "55",
         name: "Hand made Necless",
-        image: Product55,
+        image: `${baseImageUrl}/Product55',
         price: 23,
         description: "Unique and elegant handmade jewelry crafted with love and attention to detail.",
         category: "Jwellery"
@@ -378,7 +378,7 @@ const productList = [
     {
         id: "56",
         name: "Bracelet",
-        image: Product56,
+        image: `${baseImageUrl}/Product56',
         price: 21,
         description: "Unique and elegant handmade jewelry crafted with love and attention to detail.",
         category: "Jwellery"
@@ -386,7 +386,7 @@ const productList = [
     {
         id: "59",
         name: "Dan Dan Noodles",
-        image: Product59,
+        image: `${baseImageUrl}/Product59',
         price: 25,
         description: "Noodles served with spicy ground pork and sauce.",
         category: "Noodles"
@@ -394,14 +394,14 @@ const productList = [
     {
         id: "60",
         name: "bamboo pot",
-        image: Product60,
+        image: `${baseImageUrl}/Product60',
         price: 27,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.& Jute"
     },
     {
         id: "61",
         name: "Bamboo Shelf",
-        image: Product61,
+        image: `${baseImageUrl}/Product61',
         price: 30,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.",
         category: "Bamboo & Jute"
@@ -409,14 +409,14 @@ const productList = [
     {
         id: "62",
         name: "Bamboo lamp",
-        image: Product62,
+        image: `${baseImageUrl}/Product62',
         price: 20,
         description: "blending tradition with sustainability for a natural touch to your lifestyle.ute"
     },
     {
         id: "63",
         name: "Bamboo Ride",
-        image: Product63,
+        image: `${baseImageUrl}/Product63',
         price: 28,
         description: "blending tradition with sustainability for a natural touch to your lifestyle",
         category: "Bamboo & Jute"
@@ -425,7 +425,7 @@ const productList = [
     {
         id: "64",
         name: "Bird House",
-        image: Product64,
+        image: `${baseImageUrl}/Product64',
         price: 22,
         description: "blending tradition with sustainability for a natural touch to your lifestyle. ",
         category: "Bamboo & Jute"
@@ -433,7 +433,7 @@ const productList = [
     {
         id: "65",
         name: "Bamboo mirror",
-        image: Product65,
+        image: `${baseImageUrl}/Product65',
         price: 22,
         description: "blending tradition with sustainability for a natural touch to your lifestyle. ",
            category: "Bamboo & Jute"
@@ -441,7 +441,7 @@ const productList = [
 {
     id: "66",
         name: "Traditional Pinting",
-            image: Product66,
+            image: `${baseImageUrl}/Product66',
                 price: 24,
                     description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
                         category: "Painting"
@@ -450,7 +450,7 @@ const productList = [
 {
     id: "67",
         name: "Painting on bambo",
-            image: Product67,
+            image: `${baseImageUrl}/Product67,
                 price: 22,
                     description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
                         category: "Painting"
@@ -458,14 +458,14 @@ const productList = [
     , {
     id: "68",
         name: "Fish painting",
-            image: Product68,
+            image: `${baseImageUrl}/Product68',
                 price: 22,
                     description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
                         category: "Painting"
 } , {
     id: "69",
         name: "Flower Painting",
-            image: Product69,
+            image: `${baseImageUrl}/Product69,
                 price: 22,
                     description: "Exquisite handmade paintings crafted with passion, creativity, and attention to detai",
                         category: "Painting"
@@ -473,14 +473,14 @@ const productList = [
 
 ];
 
-const seedProducts = async () => {
+const seed`${baseImageUrl}/P= async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
 
-    await ProductModel.deleteMany(); // Clear existing data (optional)
-    await ProductModel.insertMany(productList);
+    await `${baseImageUrl}/ProductModel.deleteMany()'; // Clear existing data (optional)
+    await `${baseImageUrl}/ProductModel.insertMany(productList)';
 
-    console.log("✅ Products inserted successfully!");
+    console.log("✅ `${baseImageUrl}/Products' inserted successfully!");
     process.exit();
   } catch (err) {
     console.error("❌ Error inserting products:", err);
@@ -488,4 +488,4 @@ const seedProducts = async () => {
   }
 };
 
-seedProducts();
+seed`${baseImageUrl}/Products'();
